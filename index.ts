7865711e0ea16bd611fc44c63afb69bcd2853295
sumaira-shakeel <160780457+sumaira-shakeel@users.noreply.github.com>
2024-03-24 22:30:42 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! user/bin/env node  
+  
+  
+  import inquirer from "inquirer";
+const randomNumber =Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type:"number",
+        message:"please guss a number between 1-6: ",
+
+    },
+]);
+if(answers.userGussedNumber === randomNumber){
+    console.log('congratulations you guess right number:');
+}else{
+    console.log('you gussed a wrong number:');
+}
+
